c2b5c1e5e8e980d4892fcd80ea7c5fe9a364f349
jonahfarkas-create <jonah.farkas@sps.edu>
2026-05-11 16:30:22 -0400

    Project 5 day one

---
+++ P5_Lab/Matrix2x2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Matrix2x2
+{
+public: 
+    double m[2][2];
+    Matrix2x2(double a, double b, double c, double d)
+    {
+        m[0][0]=a; m[0][1]=b; m[1][0]=c; m[1][1]=d;
+  }
+
+  void print ()
+    {
+        cout << m[0][0] << " " << m[0][1] << endl;
+        cout << m[1][0] << " " << m[1][1] << endl; 
+    }
+    //Matrix2x2 add(const Matrix2x2& other)  {
+        //return Matrix2x2 (m[0][0] +other.m[0][0], m[0][1] +other.m[0][1],
+                          //m[1][0] +other.m[1][0], m[1][1] +other.m[1][1]);
+
+    Matrix2x2 multiply(const Matrix2x2& other)  {
+        return Matrix2x2 (m[0][0] *other m[0][0], m[0][1] *other.m[0][1],
+                          m[1][0] *other m[1][0], m[1][1] *other.m[1][1]);
+    }
+
+}; 
+
+int main() {
+    Matrix2x2 A(1,2,3,4), B(5,6,7,8) ; 
+    cout << "A X B: " << endl;
+    A.multiply(B) .print();
+
+    return 0;
+} 
