5a785c0a4b9cc967eadb8294a23cb8babab8b48e
jonahfarkas-create <jonah.farkas@sps.edu>
2026-05-12 09:12:50 -0400

    Added matrix mult

--- P5_Lab/Matrix2x2.cpp
+++ P5_Lab/Matrix2x2.cpp
@@ -15,13 +15,16 @@ public:
         cout << m[0][0] << " " << m[0][1] << endl;
         cout << m[1][0] << " " << m[1][1] << endl; 
     }
-    //Matrix2x2 add(const Matrix2x2& other)  {
-        //return Matrix2x2 (m[0][0] +other.m[0][0], m[0][1] +other.m[0][1],
-                          //m[1][0] +other.m[1][0], m[1][1] +other.m[1][1]);
+    Matrix2x2 add(const Matrix2x2& other)  {
+        return Matrix2x2 (m[0][0] +other.m[0][0], m[0][1] +other.m[0][1],
+                        m[1][0] +other.m[1][0], m[1][1] +other.m[1][1]); }
 
     Matrix2x2 multiply(const Matrix2x2& other)  {
-        return Matrix2x2 (m[0][0] *other m[0][0], m[0][1] *other.m[0][1],
-                          m[1][0] *other m[1][0], m[1][1] *other.m[1][1]);
+        return Matrix2x2 (m[0][0] *other.m[0][0] + m[0][1] *other.m[1][0], // first row, first column
+                          m[0][0] *other.m[0][1] + m[0][1] *other.m[1][1], // first row, second column
+                          m[1][0] *other.m[0][0] + m[1][1] *other.m[1][0], // second row, first column
+                          m[1][0] *other.m[0][1] + m[1][1] *other.m[1][1]// second row, second column
+                          );
     }
 
 }; 
@@ -31,5 +34,11 @@ int main() {
     cout << "A X B: " << endl;
     A.multiply(B) .print();
 
+    cout << "A + B: " << endl;
+    A.add(B) .print();
+
     return 0;
 } 
+
+
+
